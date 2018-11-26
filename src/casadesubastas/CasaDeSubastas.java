ceaea11a542b859911f1123b013c3b6b0f228f35
Hugo Rodríguez <hugor203@gmail.com>
2018-11-26 13:32:45 -0300

    Creación del proyecto

---
+++ src/casadesubastas/CasaDeSubastas.java
@@ -0,0 +1,16 @@
+package casadesubastas;
+
+/**
+ *
+ * @author Hugo
+ */
+public class CasaDeSubastas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
